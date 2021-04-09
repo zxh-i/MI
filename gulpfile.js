aed16856e785fd96f2d14b513288c8a432d0c165
zxh <604309690@qq.com>
2021-04-09 16:55:29 +0800

    first

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,31 @@
-const gulp=require("gulp");
-const sass=require("gulp-sass");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 // const babel=require("gulp-babel");
 //  const concat=require("gulp-concat");
-const uglify=require("gulp-uglify");
-const rename=require("gulp-rename");
-const connect=require("gulp-connect");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const connect = require("gulp-connect");
+const sourcemaps = require("gulp-sourcemaps");
 
-gulp.task("html",done=>{
+gulp.task("html", done => {
     gulp.src("*.html")
-    .pipe(gulp.dest("dist"))
-    .pipe(connect.reload());
+        .pipe(gulp.dest("dist"))
+        .pipe(connect.reload());
     done();
 });
-gulp.task("sass",done=>{
+gulp.task("sass", done => {
     gulp.src("sass/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("dist/sass"))
-    .pipe(connect.reload());
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist/css"))
+        .pipe(connect.reload());
     done();
 })
-gulp.task("js",done=>{
+gulp.task("js", done => {
     gulp.src("js/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js"))
     done();
 })
 gulp.task("img", done => {
@@ -34,21 +37,21 @@ gulp.task("img", done => {
 
 
 //监听
-gulp.task("watch",done=>{
-    gulp.watch("*.html",gulp.series("html"));
-    gulp.watch("sass/*.scss",gulp.series("sass"));
+gulp.task("watch", done => {
+    gulp.watch("*.html", gulp.series("html"));
+    gulp.watch("sass/*.scss", gulp.series("sass"));
     gulp.watch("js/*.js", gulp.series("js"));
     gulp.watch("img/*", gulp.series("img"));
     done();
 })
 //服务器
-gulp.task("server",done=>{
+gulp.task("server", done => {
     connect.server({
-        root:"dist",
-        livereload:true
+        root: "dist",
+        livereload: true
     })
     done();
 })
 
-gulp.task("build",gulp.parallel("html","sass","js","img"));
-gulp.task("default",gulp.series("build","server","watch"));
+gulp.task("build", gulp.parallel("html", "sass", "js", "img"));
+gulp.task("default", gulp.series("build", "server", "watch"));
